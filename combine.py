2d57643f0e7cd5e1d742cfa04f8d8c3460561c8e
Dbriscoe511 <extradrivespace123456@gmail.com>
2022-03-18 23:21:14 -0600

    added some test libraries

--- combine.py
+++ combine.py
@@ -1,27 +1,30 @@
 
 
-input_lib = 'test/4ms_LED.kicad_sym'
-output_lib = 'test/test.kicad_sym'
 
-with open(input_lib) as fi:
-    with open(output_lib, "r+") as fo:
-        with open(output_lib+'.bak', "w") as fo_bak:
-
+def combine_libs(input_lib,output_lib, backup):
+    with open(input_lib) as fi:
+        with open(output_lib) as fo:
             fi_lines = fi.readlines()
-            fi_lines = fi_lines[1:]
-
             fo_lines = fo.readlines()
 
+    if backup:
+        with open(output_lib+'.bak', "w") as fo_bak:
             fo_bak.seek(0)
             for line in fo_lines:
                 fo_bak.write(line)
 
-            fo_lines = fo_lines[:-1]
+            
+    with open(output_lib, "w") as fo_w:
+        fo_w.seek(0)
+        lines = fo_lines[:-1] + fi_lines[1:]
 
-            fo.seek(0)
-            lines = fo_lines + fi_lines
-            for line in lines:
-                fo.write(line)
+        print(f"{len(lines)} total lines , {len(fo_lines)} output lib, {len(fi_lines)}, input lib")
+
+        for line in lines:
+            fo_w.write(line)
+
+#test 
+combine_libs('test/RF_Mixer_dummy.kicad_sym','test/RF_Module_dummy.kicad_sym',False)
 
 
 
